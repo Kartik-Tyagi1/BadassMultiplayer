5d0513df73208eeb4aafcd0298504f2962c2c5ec
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2022-07-20 17:35:27 -0400

    Added camera and movement to character

---
+++ Source/BadassMultiplayer/Character/MultiplayerCharacter.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MultiplayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AMultiplayerCharacter::AMultiplayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	// Attach to mesh so that when we crouch or change capsule size it won't effect spring arm elevation
+	SpringArm->SetupAttachment(GetMesh());
+	SpringArm->TargetArmLength = 600.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
+
+}
+
+void AMultiplayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMultiplayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMultiplayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ThisClass::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ThisClass::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &ThisClass::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &ThisClass::LookUp);
+
+}
+
+void AMultiplayerCharacter::MoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation( 0.f, Controller->GetControlRotation().Yaw, 0.f );
+		const FVector Direction( FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X) );
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMultiplayerCharacter::MoveRight(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMultiplayerCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void AMultiplayerCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+
+
+
