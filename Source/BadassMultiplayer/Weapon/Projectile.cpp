3e9be9d8e7a08fc1bfff148fa27af80f145ed69d
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2022-09-09 14:12:45 -0400

    Created classes for projectile weapon and projectile actor

---
+++ Source/BadassMultiplayer/Weapon/Projectile.cpp
@@ -0,0 +1,28 @@
+#include "Projectile.h"
+#include "Components/BoxComponent.h"
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	SetRootComponent(CollisionBox);
+	CollisionBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic); // This will be flying through the air so it should be dynamic
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CollisionBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	CollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Block); // Block Visible Objects (aka not the camera)
+	CollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldStatic, ECollisionResponse::ECR_Block); // Static meshes like walls and stuff
+}
+
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
